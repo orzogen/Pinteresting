ae8d0b4ddee4b0f389b6bffa8e708f9af08319d1
Dan Ruswick <dan.ruswick@gmail.com>
2014-04-12 12:06:09 -0500

    added some pages and a header

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+  def home
+  	@text_array = ["helloa", "worldb", "!d"]
+  end
+  
+  def otherpage
+  	@text = "hello world"
+  	if params[:text]
+  		@text += " " + params[:text]
+  	end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Pinteresting::Application.routes.draw do
+  get "op" => "pages#otherpage"
+  get "pages/otherpage"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
